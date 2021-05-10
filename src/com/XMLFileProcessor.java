a46bc354f414c8d0b2169f82b9026d622a7bb2bc
Nakul Tirumalai <nakultirumalai@gmail.com>
2021-05-09 17:34:23 -0700

    added parser stubs and sample files

--- src/com/XMLFileProcessor.java
+++ src/com/XMLFileProcessor.java
@@ -4,21 +4,25 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.util.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.parser.*;
 
-public class CSVFileProcessor extends FileProcessor {
-    private ArrayList<String> inputLines;
+
+public class JSONFileProcessor extends FileProcessor {
     private CreditCardFactory cardFactory;
+    private JSONArray jsonObjects;
 
-    public CSVFileProcessor(String InputFileName, String OutputFileName) {
+    public JSONFileProcessor(String InputFileName, String OutputFileName) {
         super(InputFileName, OutputFileName);
-	inputLines = new ArrayList<String>();
 	cardFactory = new CreditCardFactory();
+	jsonObjects = new JSONArray();
     }
 
     @Override
     public void readCreditCardRecords(List<CreditCard> cards) {
 	/* Get all the lines from the input file */
-	getLinesFromFile();
+	getJSONObjectsFromFile();
 
 	if (inputLines.size() == 0) {
 	    /* TODO: Add an error message here */
@@ -54,50 +58,37 @@ public class CSVFileProcessor extends FileProcessor {
 	}
     }
 
-    private void getLinesFromFile() {
-	try {
-	    File fileObj = new File(inFileName);
-	    Scanner lineReader = new Scanner(fileObj);
-	    while (lineReader.hasNextLine()) {
-		String line = lineReader.nextLine();
-		/* Skip the title line in the csv file */
-		if (line.toLowerCase().startsWith("card")) {
-		    continue;
-		}
-		inputLines.add(line);
-	    }
-	    lineReader.close();
-	} catch (FileNotFoundException e) {
+    private void getJSONObjectsFromFile() {
+	JSONParser jsonParser = new JSONParser();
+	
+        try (FileReader reader = new FileReader(inFileName))
+        {
+            Object obj = jsonParser.parse(reader);
+            jsonObjects = (JSONArray) obj;
+        } catch (FileNotFoundException e) {
 	    System.out.println("Error: File " + inFileName + " could not be found");
-	} catch (Exception e) {
-	    System.out.println("Error: Other exception found: "  + e);
+        } catch (ParseException e) {
+	    System.out.println("Error: JSON parse failure: " + e);
+        } catch (Exception e) {
+	    System.out.println("Error: Other exception found: " + e);
 	}
     }
 
-    private void processLinesToCards(List<CreditCard> cards) {
-	String line = "";
-	int lineCount = 0;
+    private void processJSONObjects(List<CreditCard> cards) {
+	int objectCount = 0;
 	try {
-	    Iterator<String> lineIterator = inputLines.iterator();
-	    while (lineIterator.hasNext()) {
-		line = lineIterator.next();
-		List<String> valuesList = 
-		    Arrays.asList(line.split(",", -1));
-		if (valuesList.size() != 3) {
-		    continue;
-		}
-		String CreditCardNumber = valuesList.get(0);
-		String ExpirationDate = valuesList.get(1);
-		String HolderName = valuesList.get(2);
-		//		System.out.println("Read line: " + CreditCardNumber + "  " + HolderName + "  " + ExpirationDate);
+	    for (int idx = 0; idx < jsonObjects.length(); idx++) {
+		JSONObject jsonObj = jsonObjects.getJSONObject(idx);
+		String CreditCardNumber = jsonObj.get("CardNumber").toString();
+		String ExpirationDate = jsonObj.get("ExpirationDate").toString();
+		String ExpirationDate = jsonObj.get("NameOfCardholder").toString();
 		CreditCard creditCard = 
 		    cardFactory.getCreditCard(CreditCardNumber, HolderName, ExpirationDate);
 		cards.add(creditCard);
-		//		System.out.println("Added credit card for line: " + line);
-		lineCount++;
+		objectCount++;
 	    }
 	} catch (Exception e) {
-	    System.out.println("Error: Line " + line + " has an error, line number: " + String.valueOf(lineCount));
+	    System.out.println("Error: Object number " + objectCount + "has an error");
 	}
     }
 }
